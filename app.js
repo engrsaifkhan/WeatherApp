dba4a784eff0c49862a3627bcba85daa0af116a2
Sufy97 <saifkhan007ii@gmail.com>
2022-09-27 22:56:37 +0500

    keyboard responsive check

--- app.js
+++ app.js
@@ -26,3 +26,22 @@ document.querySelector(".windSpeed").innerHTML=`${weatherData.current.wind_kph}&
   })
 
   }
+
+
+  let viewport = document.querySelector(‘meta[name=viewport]’);
+   viewport.setAttribute(‘content’, ‘width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0’);
+
+   document.documentElement.style.setProperty(‘overflow’, ‘auto’);
+    const metaViewport = document.querySelector(‘meta[name=viewport]’);
+    metaViewport.setAttribute(‘content’, ‘height=’ + initialHeight + ‘px, width=device-width, initial-scale=1.0’)
+
+    if(window.innerHeight < initialHeight){
+
+document.documentElement.style.setProperty(‘overflow’, ‘auto’)
+viewport.setAttribute(‘content’, ‘height=’ + Security.initialHeight + ‘px, width=device-width, initial-scale=1.0’)
+}
+else if(window.innerHeight === initialHeight){
+
+  document.documentElement.style.setProperty(‘overflow’, ‘hidden’)
+viewport.setAttribute(‘content’, ‘width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0’)
+}
